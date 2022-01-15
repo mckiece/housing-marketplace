5bb3a639753a135f05996e64218abd84ce9af574
Carter McKie <mckiece@gmail.com>
2022-01-15 11:46:19 -0500

    added user sign in and reg

--- src/firebase.config.js
+++ src/firebase.config.js
@@ -1,14 +1,14 @@
 import { initializeApp } from "firebase/app";
-import { getFirestore } from "firebase/Firestore";
+import { getFirestore } from "firebase/firestore";
 
 // Your web app's Firebase configuration
 const firebaseConfig = {
-  apiKey: "AIzaSyBeAFla8JLNBVpi3lGWEJR4Voauo4ohNDo",
-  authDomain: "house-marketplace-app-29ce5.firebaseapp.com",
-  projectId: "house-marketplace-app-29ce5",
-  storageBucket: "house-marketplace-app-29ce5.appspot.com",
-  messagingSenderId: "3085991166",
-  appId: "1:3085991166:web:52f0d9a867020c605239af"
+	apiKey: "AIzaSyBeAFla8JLNBVpi3lGWEJR4Voauo4ohNDo",
+	authDomain: "house-marketplace-app-29ce5.firebaseapp.com",
+	projectId: "house-marketplace-app-29ce5",
+	storageBucket: "house-marketplace-app-29ce5.appspot.com",
+	messagingSenderId: "3085991166",
+	appId: "1:3085991166:web:52f0d9a867020c605239af",
 };
 
 // Initialize Firebase
--- src/pages/SignIn.jsx
+++ src/pages/SignIn.jsx
@@ -1,8 +1,101 @@
+import { useState } from "react";
+import { toast } from "react-toastify";
+import { Link, useNavigate } from "react-router-dom";
+import { getAuth, signInWithEmailAndPassword } from "firebase/auth";
+import { ReactComponent as ArrowRightIcon } from "../assets/svg/keyboardArrowRightIcon.svg";
+import visibilityIcon from "../assets/svg/visibilityIcon.svg";
+
 function SignIn() {
+	const [showPassword, setShowPassword] = useState(false);
+	const [formData, setformData] = useState({
+		email: "",
+		password: "",
+	});
+	const { email, password } = formData;
+
+	const navigate = useNavigate();
+
+	const onChange = (e) => {
+		setformData((prevState) => ({
+			...prevState,
+			[e.target.id]: e.target.value,
+		}));
+	};
+
+	const onSubmit = async (e) => {
+		e.preventDefault();
+
+		try {
+			const auth = getAuth();
+
+			const userCredential = await signInWithEmailAndPassword(
+				auth,
+				email,
+				password
+			);
+
+			if (userCredential.user) navigate("/");
+		} catch (error) {
+			toast.error("Bad User Credentials");
+		}
+	};
+
 	return (
-		<div>
-			<h1>Sign In</h1>
-		</div>
+		<>
+			<div className="pageContainer">
+				<header>
+					<p className="pageHeader">Welcome Back!</p>
+				</header>
+
+				<form onSubmit={onSubmit}>
+					<input
+						type="email"
+						placeholder="Email"
+						id="email"
+						className="emailInput"
+						value={email}
+						onChange={onChange}
+					/>
+					<div className="passwordInputDiv">
+						<input
+							type={showPassword ? "text" : "password"}
+							className="passwordInput"
+							placeholder="Password"
+							id="password"
+							value={password}
+							onChange={onChange}
+						/>
+						<img
+							src={visibilityIcon}
+							alt="show password"
+							className="showPassword"
+							onClick={() =>
+								setShowPassword((prevState) => !prevState)
+							}
+						/>
+					</div>
+					<Link to="/forgot-password" className="forgotPasswordLink">
+						Forgot Password
+					</Link>
+					<div className="signInBar">
+						<p className="signInText">Sign In</p>
+						<button className="signInButton">
+							<ArrowRightIcon
+								fill="#fffff"
+								width="34px"
+								height="34"
+							/>
+						</button>
+					</div>
+				</form>
+
+				{/* Google OAuth */}
+
+				<Link to="/sign-up" className="registerLink">
+					Sign Up Instead
+				</Link>
+			</div>
+		</>
 	);
 }
 

4159f9f0a44d83be35e212cf7cdae73f64e673f6
mckiece <39381269+mckiece@users.noreply.github.com>
2022-03-30 11:46:41 -0400

    Remove OAuth

--- src/pages/SignUp.jsx
+++ src/pages/SignUp.jsx
@@ -10,7 +10,6 @@ import { setDoc, doc, serverTimestamp } from "firebase/firestore";
 import { db } from "../firebase.config";
 import { ReactComponent as ArrowRightIcon } from "../assets/svg/keyboardArrowRightIcon.svg";
 import visibilityIcon from "../assets/svg/visibilityIcon.svg";
-import OAuth from "../components/OAuth";
 
 function SignUp() {
 	const [showPassword, setShowPassword] = useState(false);
@@ -114,8 +113,6 @@ function SignUp() {
 						</button>
 					</div>
 				</form>
-
-				<OAuth />
 
 				<Link to="/sign-in" className="registerLink">
 					Sign In Instead
